7699bbb39e3393375ad47f3439131411c0381c1a
Emanoel Aleixo <emanoel.ass@hotmail.com>
2025-06-28 18:36:51 -0300

    inicio de desafio

---
+++ desafio-controle/src/Contador.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro:");
+        int primeiroParametro = sn.nextInt();
+        System.out.println("Digite o segundo parâmetro:");
+        int segundoParametro = sn.nextInt();
+
+        
+
+        sn.close();
+    }
+}
