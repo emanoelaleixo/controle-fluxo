924d422f5da55ce0f47fa1a9801296fea2e5033f
Emanoel Aleixo <emanoel.ass@hotmail.com>
2025-07-01 21:09:16 -0300

    inclusão de execeção e finalização de desafio

--- desafio-controle/src/Contador.java
+++ desafio-controle/src/Contador.java
@@ -8,8 +8,24 @@ public class Contador {
         System.out.println("Digite o segundo parâmetro:");
         int segundoParametro = sn.nextInt();
 
-        
+        try {
+            contar(primeiroParametro, segundoParametro);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
 
         sn.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i <= contagem ; i++) {
+            System.out.println(i);
+        }
     }
 }
